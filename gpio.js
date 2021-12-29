b20483892408f91a6628f7cece74688d4e0a3802
magasb1 <magasb1@gmail.com>
2021-12-29 14:13:25 +0100

    feilretting

--- gpio.js
+++ gpio.js
@@ -2,28 +2,11 @@ const path = require('path')
 const Gpio = require('onoff').Gpio
 require('dotenv').config({ path: path.resolve(__dirname, '.env') })
 
-const relay = new Gpio(process.env.RELAY_GIPO_PIN, 'high');
-relay.read()
-    .then(() => {
-        console.log(`ON (${process.env.RELAY_ON})`)
-        relay.writeSync(process.env.RELAY_ON)
-    })
-    .then(() => {
-        setTimeout(() => {
-            console.log(`OFF (${process.env.RELAY_OFF})`)
-            relay.writeSync(process.env.RELAY_OFF)
-        }, process.env.RELAY_TIMEOUT)
-    })
-    .then(() => {
-        setTimeout(() => {
-            console.log(`UNEXPORT`)
-            relay.unexport();
-        }, process.env.RELAY_TIMEOUT)
-    })
-    .then(() => {
-        return false; //res.json({"Status": "Ok", "Message": "Door triggered"})
-    })
-    .catch(err => console.log(err))
+const relay = new Gpio(process.env.RELAY_GPIO_PIN, 'high');
+relay.writeSync(process.env.RELAY_ON)
+setTimeout(() => {
+    relay.writeSync(process.env.RELAY_OFF)
+}, process.env.RELAY_TIMEOUT)
 
 
 
--- src/controllers/gpio.controller.js
+++ src/controllers/gpio.controller.js
@@ -5,20 +5,12 @@ require('dotenv').config({ path: path.resolve(__dirname, '../../.env') })
 module.exports = {
   activateRelay: (req, res, next) => {
     const relay = new Gpio(process.env.RELAY_GPIO_PIN, 'high');
-    relay.read()
-      .then(() => {
-        console.log(`ON (${process.env.RELAY_ON})`)
-        relay.write(process.env.RELAY_ON)
-      })
-      .then(() => {
-        setTimeout(() => {
-          console.log(`OFF (${process.env.RELAY_OFF})`)
-          relay.write(process.env.RELAY_OFF)
-        }, process.env.RELAY_TIMEOUT)
-      })
-      .then(() => {
-        return res.json({ "Status": "Ok", "Message": "Door triggered" })
-      })
-      .catch(err => console.log(err))
+    
+    relay.writeSync(process.env.RELAY_ON)
+    setTimeout(() => {
+      relay.writeSync(process.env.RELAY_OFF)
+    }, process.env.RELAY_TIMEOUT)
+
+    return res.json({ "Status": "Ok", "Message": "Door triggered" })
   }
 }
