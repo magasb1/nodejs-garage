8d46c11fc1f93922e62f8813e86e3527e1340349
magasb1 <magasb1@gmail.com>
2021-12-29 12:59:20 +0100

    feilretting

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-require('dotenv').config
+require('dotenv').config({ path: '../.env' })
 const express = require('express')
 //const { PrismaClient } = require('@prisma/client')
 const compression = require('compression');
--- src/controllers/gpio.controller.js
+++ src/controllers/gpio.controller.js
@@ -1,4 +1,4 @@
-require('dotenv').config
+require('dotenv').config({ path: '../../.env' })
 const Gpio = require('onoff').Gpio
 
 const activateRelay = (req, res, next) => {
