a427cc6b5bd8dfbfa38a1078dedc4cdd000854c1
magasb1 <magasb1@gmail.com>
2021-12-29 12:55:58 +0100

    feilretting

---
+++ gpio.js
@@ -0,0 +1,19 @@
+require('dotenv').config({ path: '.env' })
+const Gpio = require('onoff').Gpio
+
+const relay = new Gpio(process.env.RELAY_GIPO_PIN, 'out');
+relay.read()
+    .then(() => {
+        console.log(`ON (${process.env.RELAY_ON})`)
+        relay.writeSync(process.env.RELAY_ON)
+        })
+    .then(() => setTimeout(() => {
+        console.log(`OFF (${process.env.RELAY_OFF})`)
+        relay.writeSync(process.env.RELAY_OFF)
+    }, process.env.RELAY_TIMEOUT))
+    .catch(err => console.log(err))
+
+
+process.on('SIGINT', () => {
+  relay.unexport();
+})
--- src/controllers/gpio.controller.js
+++ src/controllers/gpio.controller.js
@@ -2,17 +2,22 @@ require('dotenv').config
 const Gpio = require('onoff').Gpio
 
 const activateRelay = () => {
-    const relay = new Gpio(process.env.RELAY_GIPO_PIN, 'out');
-    relay.read()
-      .then(() => console.log(`ON`) & relay.write(process.env.RELAY_ON))
-      .then(() => setTimeout(() => { 
-          console.log(`OFF`)
-          relay.write(process.env.RELAY_OFF) 
-          setTimeout(() => {
-            relay.unexport()
-          }, process.env.RELAY_TIMEOUT)
-        }, process.env.RELAY_TIMEOUT ))
-      .catch( err => console.log(err))
+  const relay = new Gpio(process.env.RELAY_GIPO_PIN, 'out');
+  relay.read()
+    .then(() => {
+      console.log(`ON (${process.env.RELAY_ON})`)
+      relay.writeSync(process.env.RELAY_ON)
+    })
+    .then(() => setTimeout(() => {
+      console.log(`OFF (${process.env.RELAY_OFF})`)
+      relay.writeSync(process.env.RELAY_OFF)
+    }, process.env.RELAY_TIMEOUT))
+    .catch(err => console.log(err))
+
+
+  process.on('SIGINT', () => {
+    relay.unexport();
+  })
 };
 
 module.exports = activateRelay;
