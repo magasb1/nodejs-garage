7aa26e96a3e368132e5263c3fa5b03a53570303b
magasb1 <magasb1@gmail.com>
2021-12-29 13:49:01 +0100

    feilretting

--- src/controllers/gpio.controller.js
+++ src/controllers/gpio.controller.js
@@ -4,16 +4,16 @@ require('dotenv').config({ path: path.resolve(__dirname, '../../.env') })
 
 module.exports = {
   activateRelay: (req, res, next) => {
-    const relay = new Gpio(process.env.RELAY_GIPO_PIN, 'high');
+    const relay = new Gpio(process.env.RELAY_GPIO_PIN, 'high');
     relay.read()
       .then(() => {
         console.log(`ON (${process.env.RELAY_ON})`)
-        relay.writeSync(process.env.RELAY_ON)
+        relay.write(process.env.RELAY_ON)
       })
       .then(() => {
         setTimeout(() => {
           console.log(`OFF (${process.env.RELAY_OFF})`)
-          relay.writeSync(process.env.RELAY_OFF)
+          relay.write(process.env.RELAY_OFF)
         }, process.env.RELAY_TIMEOUT)
       })
       .then(() => {
