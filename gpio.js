3debc39ae6408fc323537b66d4739d661e422bc8
magasb1 <magasb1@gmail.com>
2021-12-29 13:46:20 +0100

    feilretting

--- gpio.js
+++ gpio.js
@@ -1,19 +1,34 @@
-require('dotenv').config({ path: '.env' })
+const path = require('path')
 const Gpio = require('onoff').Gpio
+require('dotenv').config({ path: path.resolve(__dirname, '.env') })
 
-const relay = new Gpio(process.env.RELAY_GIPO_PIN, 'out');
+const relay = new Gpio(process.env.RELAY_GIPO_PIN, 'high');
 relay.read()
     .then(() => {
         console.log(`ON (${process.env.RELAY_ON})`)
         relay.writeSync(process.env.RELAY_ON)
-        })
-    .then(() => setTimeout(() => {
-        console.log(`OFF (${process.env.RELAY_OFF})`)
-        relay.writeSync(process.env.RELAY_OFF)
-    }, process.env.RELAY_TIMEOUT))
+    })
+    .then(() => {
+        setTimeout(() => {
+            console.log(`OFF (${process.env.RELAY_OFF})`)
+            relay.writeSync(process.env.RELAY_OFF)
+        }, process.env.RELAY_TIMEOUT)
+    })
+    .then(() => {
+        setTimeout(() => {
+            console.log(`UNEXPORT`)
+            relay.unexport();
+        }, process.env.RELAY_TIMEOUT)
+    })
+    .then(() => {
+        return false; //res.json({"Status": "Ok", "Message": "Door triggered"})
+    })
     .catch(err => console.log(err))
 
 
-process.on('SIGINT', () => {
-  relay.unexport();
-})
+
+
+
+
+
+
--- src/controllers/gpio.controller.js
+++ src/controllers/gpio.controller.js
@@ -4,22 +4,26 @@ require('dotenv').config({ path: path.resolve(__dirname, '../../.env') })
 
 module.exports = {
   activateRelay: (req, res, next) => {
-    const relay = new Gpio(process.env.RELAY_GIPO_PIN, 'out');
+    const relay = new Gpio(process.env.RELAY_GIPO_PIN, 'high');
     relay.read()
       .then(() => {
         console.log(`ON (${process.env.RELAY_ON})`)
         relay.writeSync(process.env.RELAY_ON)
       })
-      .then(() => setTimeout(() => {
-        console.log(`OFF (${process.env.RELAY_OFF})`)
-        relay.writeSync(process.env.RELAY_OFF)
+      .then(() => {
+        setTimeout(() => {
+          console.log(`OFF (${process.env.RELAY_OFF})`)
+          relay.writeSync(process.env.RELAY_OFF)
+        }, process.env.RELAY_TIMEOUT)
+      })
+      .then(() => {
         setTimeout(() => {
           console.log(`UNEXPORT`)
           relay.unexport();
         }, process.env.RELAY_TIMEOUT)
-      }, process.env.RELAY_TIMEOUT))
+      })
       .then(() => {
-        return res.json({"Status": "Ok", "Message": "Door triggered"})
+        return res.json({ "Status": "Ok", "Message": "Door triggered" })
       })
       .catch(err => console.log(err))
   }
