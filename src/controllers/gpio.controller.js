84edaa89ff7c810925274b80b659dff4fa556ed7
magasb1 <magasb1@gmail.com>
2021-12-30 16:09:40 +0100

    Upd

--- src/controllers/gpio.controller.js
+++ src/controllers/gpio.controller.js
@@ -24,14 +24,7 @@ module.exports = {
 
   sensorStatus:  catchAsync( () => {
     const SENSOR_PIN = process.env.SENSOR_GPIO_PIN || 15
-
     const sensor = new Gpio(SENSOR_PIN, 'in', 'both');
-    sensor.read()
-      .then(value => {
-        return value
-      })
-      .catch(err => {
-        return err
-      })
+    sensor.read().then((value) => { return value })
   })
 }
