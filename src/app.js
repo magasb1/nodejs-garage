0893001476269fe778d7af1aa9f55f901c3ec676
magasb1 <magasb1@gmail.com>
2021-12-30 17:01:19 +0100

    Upd

--- src/app.js
+++ src/app.js
@@ -18,7 +18,22 @@ app.use(morgan.successHandler);
 app.use(morgan.errorHandler);
 
 // set security HTTP headers
-app.use(helmet());
+app.use(helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"], 
+        scriptSrc: ["'self'", 'ourAuth0domain.us.auth0.com'],
+        styleSrc: ["'self'", 'https://fonts.googleapis.com', "'unsafe-inline'"],
+        imgSrc: ["'self'", 'https://ourAuth0domain.us.auth0.com', 'data:'],
+        connectSrc: ["'self'", 'https://ourAuth0domain.us.auth0.com/oauth/token'],
+        fontSrc: ["'self'", 'https://fonts.gstatic.com'],
+        objectSrc: ["'self'"],
+        mediaSrc: ["'self'"],
+        frameSrc: ["'self'"]
+      },
+      reportOnly: true
+    }
+  }));
 
 // parse json request body
 app.use(express.json());
