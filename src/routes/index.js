4df5240b25bf710eaf8c34e663a60d8865932e0f
magasb1 <magasb1@gmail.com>
2021-12-30 16:11:34 +0100

    Upd

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,9 +6,9 @@ const router = express.Router();
 router
     .route('/')
     .get( (req, res, next) => {
-        console.log(`Door status: ${gpioController.sensorStatus}`)
+        console.log(`Door status: ${gpioController.sensorStatus()}`)
         res.render("index", {
-            sensorStatus: gpioController.sensorStatus ? "Lukket" : "Åpen"
+            sensorStatus: gpioController.sensorStatus() ? "Lukket" : "Åpen"
         })
     });
 
