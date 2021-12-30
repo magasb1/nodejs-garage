f39e1b4b93981832f50eb316e693b05a1e2228f1
magasb1 <magasb1@gmail.com>
2021-12-30 16:34:58 +0100

    Upd

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,7 +7,7 @@ router
     .route('/')
     .get( (req, res, next) => {
         res.render("index", {
-            sensorStatus: "Unknown"
+            sensorStatus: gpioController.sensorStatus()
         })
     });
 
